43352efc16d3289ce509cf5c937b4992740bb5fe
GeorgTheVic <georgthevic@gmail.com>
2024-10-31 15:12:01 +0500

    приступить к стилизации

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom"
 
 const Header = () => {
     return (
-        <div className="navbar">
-            <div className="navbar__links">
-                <Link to='/account'>Account</Link>
-                <Link to='/tasks'>Tasks</Link>
-                <Link to='/friends'>Friends</Link>
-                <Link to='/budget'>Budget</Link>
+        <>
+            <div className="header">
+                <Link to='/account' >Account</Link>
+                <Link to='/tasks' >Tasks</Link>
+                <Link to='/friends' >Friends</Link>
+                <Link to='/budget' >Budget</Link>
             </div>
-        </div>
+        </>
     )
 }
 
